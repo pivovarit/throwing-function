095da27bad0b6cc0c2ce2a328501ec274bdc469f
Grzegorz Piwowarek <gpw@touk.pl>
2015-11-27 11:36:47 +0100

    ThrowingBiConsumer

---
+++ src/main/java/touk/pl/function/ThrowingBiConsumer.java
@@ -0,0 +1,28 @@
+package touk.pl.function;
+
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+@FunctionalInterface
+public interface ThrowingBiConsumer<T, U, E extends Exception> {
+    void accept(T t, U u) throws E;
+
+    default ThrowingBiConsumer<T, U, E> andThenConsume(final ThrowingBiConsumer<? super T, ? super U, E> after) {
+        Objects.requireNonNull(after);
+
+        return (arg1, arg2) -> {
+            accept(arg1, arg2);
+            after.accept(arg1, arg2);
+        };
+    }
+
+    default BiConsumer<T, U> wrappedWithRuntimeException() {
+        return (arg1, arg2) -> {
+            try {
+                accept(arg1, arg2);
+            } catch (final Exception e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
