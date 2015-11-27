990e76b7add68c904fc5de5c8af696156ebd521d
Grzegorz Piwowarek <gpw@touk.pl>
2015-11-27 12:26:56 +0100

    ThrowingFunctionTest

---
+++ src/test/java/touk/pl/function/ThrowingFunctionTest.java
@@ -0,0 +1,76 @@
+package touk.pl.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class ThrowingFunctionTest {
+
+    @Test
+    public void shouldCompose() throws Exception {
+        // given
+        final ThrowingFunction<Integer, Integer, Exception> f1 = i -> 2 * i;
+        final ThrowingFunction<Integer, Integer, Exception> f2 = i -> i + 10;
+
+        // when
+        final Integer result = f1.compose(f2).apply(3);
+
+        // then
+        assertThat(result).isEqualTo(26);
+    }
+
+
+    @Test
+    public void shouldAndThen() throws Exception {
+        // given
+        final ThrowingFunction<Integer, Integer, Exception> f1 = i -> 2 * i;
+        final ThrowingFunction<Integer, Integer, Exception> f2 = i -> i + 10;
+
+        // when
+        final Integer result = f1.andThen(f2).apply(3);
+
+        // then
+        assertThat(result).isEqualTo(16);
+    }
+
+    @Test
+    public void shouldReturnOptional() throws Exception {
+        // given
+        final ThrowingFunction<Integer, Integer, Exception> f1 = ThrowingFunction.identity();
+
+        // when
+        final Optional<Integer> result = f1.toOptionalFunction().apply(42);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    public void shouldReturnEmptyOptional() throws Exception {
+        // given
+        final ThrowingFunction<Integer, Integer, Exception> f1 = i -> {
+            throw new Exception();
+        };
+
+        // when
+        final Optional<Integer> result = f1.toOptionalFunction().apply(42);
+
+        // then
+        assertThat(result.isPresent()).isFalse();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldWrapInRuntimeEx() throws Exception {
+        // given
+        final ThrowingFunction<Integer, Integer, Exception> f1 = i -> {
+            throw new Exception();
+        };
+
+        // when
+        f1.wrappedWithRuntimeException().apply(42);
+
+        // then RuntimeException is thrown
+    }
+}
