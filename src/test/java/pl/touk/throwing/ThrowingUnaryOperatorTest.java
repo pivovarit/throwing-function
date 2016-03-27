da42e658ef33614e7bf681ac44f02f22e5ba793b
Grzegorz Piwowarek <gpw@touk.pl>
2016-03-27 19:30:34 +0200

    ThrowingUnaryOperator missing tests

---
+++ src/test/java/pl/touk/throwing/ThrowingUnaryOperatorTest.java
@@ -0,0 +1,42 @@
+package pl.touk.throwing;
+
+import org.junit.Test;
+import pl.touk.throwing.exception.WrappedException;
+
+import java.io.IOException;
+
+public class ThrowingUnaryOperatorTest {
+
+    @Test
+    public void shouldApply() throws Exception {
+        // given
+        ThrowingUnaryOperator<Integer, IOException> op = i -> i;
+
+        // when
+        op.apply(42);
+
+        // then no exception thrown
+    }
+
+    @Test
+    public void shouldApplyUnchecked() throws Exception {
+        // given
+        ThrowingUnaryOperator<Integer, IOException> op = i -> i;
+
+        // when
+        ThrowingUnaryOperator.unchecked(op).apply(42);
+
+        // then no exception thrown
+    }
+
+    @Test(expected = WrappedException.class)
+    public void shouldApplyUncheckedAndThrow() throws Exception {
+        // given
+        ThrowingUnaryOperator<Integer, IOException> op = i -> { throw new IOException(); };
+
+        // when
+        ThrowingUnaryOperator.unchecked(op).apply(42);
+
+        // then no exception thrown
+    }
+}
