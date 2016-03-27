3bdd60e2864a3fb27ef0ba87a4d93080fdb1f4dc
Grzegorz Piwowarek <gpw@touk.pl>
2016-03-27 18:29:35 +0200

    ThrowingSupplier missing tests

---
+++ src/test/java/pl/touk/throwing/ThrowingSupplierTest.java
@@ -0,0 +1,71 @@
+package pl.touk.throwing;
+
+import org.junit.Test;
+import pl.touk.throwing.exception.WrappedException;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThrowingSupplierTest {
+
+    @Test
+    public void shouldGet() throws Exception {
+        // given
+        final ThrowingSupplier<Integer, IOException> supplier = () -> 42;
+
+        // when
+        final Integer result = supplier.get();
+
+        // then
+        assertThat(result).isEqualTo(42);
+    }
+
+    @Test
+    public void shouldGetAsFunction() throws Exception {
+        // given
+        final ThrowingSupplier<Integer, IOException> supplier = () -> 42;
+
+        // when
+        final ThrowingFunction<Void, Integer, IOException> result = supplier.asFunction();
+
+        // then
+        assertThat(result.apply(null)).isEqualTo(42);
+    }
+
+    @Test
+    public void shouldLiftAndGet() throws Exception {
+        // given
+        final ThrowingSupplier<Integer, IOException> supplier = () -> 42;
+
+        // when
+        final Optional<Integer> result = supplier.lift().get();
+
+        // then
+        assertThat(result).isPresent();
+    }
+
+    @Test
+    public void shouldLiftAndGetEmpty() throws Exception {
+        // given
+        final ThrowingSupplier<Integer, IOException> supplier = () -> { throw new IOException(); };
+
+        // when
+        final Optional<Integer> result = supplier.lift().get();
+
+        // then
+        assertThat(result).isEmpty();
+    }
+
+    @Test(expected = WrappedException.class)
+    public void shouldGetUnchecked() throws Exception {
+        // given
+        final ThrowingSupplier<Integer, IOException> supplier = () -> { throw new IOException(); };
+
+        // when
+        supplier.unchecked().get();
+
+        // then exception is thrown
+    }
+}
