4e4aecbf6f4e22be651ea926a951dd116f09f1f9
pivovarit <gpiwowarek@gmail.com>
2018-03-31 19:38:49 +0200

    Rename package structure

--- src/main/java/pl/touk/throwing/exception/WrappedException.java
+++
@@ -1,7 +0,0 @@
-package pl.touk.throwing.exception;
-
-public class WrappedException extends RuntimeException {
-    public WrappedException(Throwable cause) {
-        super(cause.getMessage(), cause);
-    }
-}
