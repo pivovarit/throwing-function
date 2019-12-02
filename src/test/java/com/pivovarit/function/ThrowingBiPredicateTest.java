69f098ece91e69b96b6724ebb29fba8bd254bd64
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2019-12-02 19:14:31 +0100

    Fix ThrowingBiPredicate

--- src/test/java/com/pivovarit/function/ThrowingBiPredicateTest.java
+++ src/test/java/com/pivovarit/function/ThrowingBiPredicateTest.java
@@ -20,65 +20,6 @@ class ThrowingBiPredicateTest {
     }
 
     @Test
-    void shouldTestOR() throws Exception {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> pTrue = (i, j) -> true;
-        ThrowingBiPredicate<Integer, Integer, Exception> pFalse = (i, j) -> false;
-
-        // then
-        assertThat(pTrue.or(pFalse).test(42, 0)).isTrue();
-        assertThat(pTrue.or(pTrue).test(42, 0)).isTrue();
-        assertThat(pFalse.or(pTrue).test(42, 0)).isTrue();
-        assertThat(pFalse.or(pFalse).test(42, 0)).isFalse();
-    }
-
-    @Test
-    void shouldTestXOR() throws Exception {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> pTrue = (i, j) -> true;
-        ThrowingBiPredicate<Integer, Integer, Exception> pFalse = (i, j) -> false;
-
-        // then
-        assertThat(pTrue.xor(pFalse).test(42, 0)).isTrue();
-        assertThat(pTrue.xor(pTrue).test(42, 0)).isFalse();
-        assertThat(pFalse.xor(pTrue).test(42, 0)).isTrue();
-        assertThat(pFalse.xor(pFalse).test(42, 0)).isFalse();
-    }
-
-    @Test
-    void shouldTestAND() throws Exception {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> pTrue = (i, j) -> true;
-        ThrowingBiPredicate<Integer, Integer, Exception> pFalse = (i, j) -> false;
-
-        // then
-        assertThat(pTrue.and(pFalse).test(42, 0)).isFalse();
-        assertThat(pTrue.and(pTrue).test(42, 0)).isTrue();
-        assertThat(pFalse.and(pTrue).test(42, 0)).isFalse();
-        assertThat(pFalse.and(pFalse).test(42, 0)).isFalse();
-    }
-
-    @Test
-    void shouldTestNegate() throws Exception {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> pTrue = (i, j) -> true;
-        ThrowingBiPredicate<Integer, Integer, Exception> pFalse = (i, j) -> false;
-
-        // then
-        assertThat(pTrue.negate().test(42, 0)).isFalse();
-        assertThat(pFalse.negate().test(42, 0)).isTrue();
-    }
-
-    @Test
-    void shouldTestAsFunction() throws Exception {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> p = (i, j) -> true;
-
-        // then
-        assertThat(p.asFunction().apply(1, 2)).isTrue();
-    }
-
-    @Test
     void shouldWrapInRuntimeExWhenUsingStandardUtilsFunctions() {
         Exception cause = new Exception("some message");
 
@@ -92,16 +33,5 @@ class ThrowingBiPredicateTest {
           .isInstanceOf(RuntimeException.class)
           .hasMessage(cause.getMessage())
           .hasCause(cause);
-    }
-
-    @Test
-    void shouldTestWhenUsingUncheck() {
-        // given
-        ThrowingBiPredicate<Integer, Integer, Exception> predicate = (i, j) -> true;
-
-        // when
-        predicate.uncheck().test(1, 2);
-
-        // then no exception is thrown
     }
 }
